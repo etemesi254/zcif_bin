7fa9e7fa0a494692a348de954101f65ee2775d45
caleb <etemesicaleb@gmail.com>
2022-05-05 19:20:51 +0300

    Added rstdc, new chanfes to reflect library

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{arg, Command};
-use std::{time::Instant};
+use std::{arch::x86_64::_rdtsc, time::Instant};
 use zcif::{fse_compress, fse_decompress, huff_compress, huff_decompress};
 
 fn main() {
@@ -14,34 +14,35 @@ fn main() {
             arg!(-z --huff <file> "Get Huffman encoding and decoding speeds for <file> ")
                 .required(false),
         )
-        .arg(arg!(-r --runs <run> "Run the encoder/decoder <run> number of times").required(false
-        ).default_value("20"))
+        .arg(
+            arg!(-r --runs <run> "Run the encoder/decoder <run> number of times")
+                .required(false)
+                .default_value("20"),
+        )
         .get_matches();
     let runs = clp.value_of("runs").unwrap().parse::<usize>().unwrap_or(20);
 
     if clp.value_of("fse").is_some() {
         println!("Opening file: {:?}", clp.value_of("fse").unwrap());
-        _fse(clp.value_of("fse").unwrap(),runs);
+        unsafe { _fse(clp.value_of("fse").unwrap(), runs) };
     } else if clp.value_of("huff").is_some() {
         println!("Opening file: {:?}", clp.value_of("huff").unwrap());
-        _huff(clp.value_of("huff").unwrap(),runs);
+        unsafe { _huff(clp.value_of("huff").unwrap(), runs) };
     }
     // _fse();
 
     // _huff();
 }
 
-fn _huff(file: &str,runs:usize) {
+unsafe fn _huff(file: &str, runs: usize) {
     let writer = Vec::with_capacity(2);
     let mut writer = std::io::Cursor::new(writer);
-    
+
     writer.set_position(0);
 
     let mut reader = std::io::Cursor::new(std::fs::read(file).unwrap());
 
-    
     let mut new_writer = Vec::<u8>::with_capacity(85 * (1 << 20));
-
 
     println!("In memory buffer compression Huffman, {} runs", runs);
     for _ in 0..runs {
@@ -58,9 +59,28 @@ fn _huff(file: &str,runs:usize) {
         writer.set_position(0);
 
         let start_decomp = Instant::now();
-        huff_decompress(&mut writer, &mut new_writer).unwrap();
+        let start = {
+            #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+            {
+                _rdtsc()
+            }
+            #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+            {
+                0
+            }
+        };
+        huff_decompress(writer.get_ref(), &mut new_writer).unwrap();
+        let stop = {
+            #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+            {
+                _rdtsc()
+            }
+            #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+            {
+                0
+            }
+        };
         let stop_decomp = Instant::now();
-
 
         eprint!(
             "\r {} bytes -> {} bytes ({:.2}%)  [{:.4} to 1] ",
@@ -70,6 +90,10 @@ fn _huff(file: &str,runs:usize) {
             reader.get_ref().len() as f64 / (writer.get_ref().len() as f64)
         );
         eprint!(
+            "{:.2?} cycles per byte ",
+            (stop - start) as f64 / (new_writer.len()) as f64
+        );
+        eprint!(
             " {:.2} MB/s",
             reader.get_ref().len() as f64
                 / ((1 << 20) as f64 * (stop_compress - start_compress).as_secs_f64())
@@ -77,10 +101,9 @@ fn _huff(file: &str,runs:usize) {
 
         eprint!(
             " {:.2} MB/s ",
-            new_writer.len() as f64 
+            new_writer.len() as f64
                 / ((1 << 20) as f64 * (stop_decomp - start_decomp).as_secs_f64())
         );
-        
     }
     println!();
 
@@ -88,7 +111,7 @@ fn _huff(file: &str,runs:usize) {
     assert!(reader.get_ref().eq(&new_writer), "Bad decoder");
 }
 
-fn _fse(file: &str,runs:usize) {
+unsafe fn _fse(file: &str, runs: usize) {
     let writer = Vec::with_capacity(2);
     let mut writer = std::io::Cursor::new(writer);
     writer.set_position(0);
@@ -97,7 +120,6 @@ fn _fse(file: &str,runs:usize) {
 
     let mut new_writer = Vec::<u8>::with_capacity(85 * (1 << 20));
 
-    
     println!("In memory buffer compression FSE, {} runs", runs);
     for _ in 0..runs {
         writer.set_position(0);
@@ -113,9 +135,29 @@ fn _fse(file: &str,runs:usize) {
         writer.set_position(0);
 
         let start_decomp = Instant::now();
-        fse_decompress(&mut writer, &mut new_writer).unwrap();
-        let stop_decomp = Instant::now();
+        let start = {
+            #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+            {
+                _rdtsc()
+            }
+            #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+            {
+                0
+            }
+        };
+        fse_decompress(writer.get_ref(), &mut new_writer).unwrap();
+        let stop = {
+            #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+            {
+                _rdtsc()
+            }
+            #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+            {
+                0
+            }
+        };
 
+        let stop_decomp = Instant::now();
 
         eprint!(
             "\r {} bytes -> {} bytes ({:.2}%)  [{:.4} to 1] ",
@@ -125,6 +167,11 @@ fn _fse(file: &str,runs:usize) {
             reader.get_ref().len() as f64 / (writer.get_ref().len() as f64)
         );
         eprint!(
+            "{:.2?} cycles per byte ",
+            (stop - start) as f64 / (new_writer.len()) as f64
+        );
+
+        eprint!(
             "  {:.2} MB/s",
             reader.get_ref().len() as f64
                 / ((1 << 20) as f64 * (stop_compress - start_compress).as_secs_f64())
@@ -132,14 +179,11 @@ fn _fse(file: &str,runs:usize) {
 
         eprint!(
             " {:.2} MB/s ",
-            new_writer.len() as f64 
+            new_writer.len() as f64
                 / ((1 << 20) as f64 * (stop_decomp - start_decomp).as_secs_f64())
         );
-        
     }
     println!();
-
-
 
     assert!(reader.get_ref().eq(&new_writer), "Bad decoder");
 }
